0e607a54d428b0df5634a12e27bd95a53c612c83
Rajan Sharma <82056580+Immortal-codes@users.noreply.github.com>
2022-09-12 16:15:17 +0530

    Add files via upload

---
+++ josephusProblem.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n=3,k=2;
+
+    for (int i = 1; i <=n; i++)
+    {
+        if (i==(k+1))
+        {
+            cout<<i;
+        }
+        
+    }
+    
+
+
+}
